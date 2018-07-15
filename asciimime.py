319c7690734cc464f18e3a13bf655ae9c89a0e52
tpltnt <1172976+tpltnt@users.noreply.github.com>
2018-07-15 23:00:35 +0200

    added cli arguments and app registration

--- asciimime.py
+++ asciimime.py
@@ -4,6 +4,8 @@ This is a simple mastodon bot. It pretends to be
 a mime and mirrors the (sentiment of the) toots
 of a user.
 """
+import argparse
+from mastodon import Mastodon
 from textblob import TextBlob
 
 def text_to_emoticon(txt):
@@ -26,3 +28,32 @@ def text_to_emoticon(txt):
 
     # subjectivity: [0.0, 1.0] objective .. subjective
     return ""
+
+
+def register_app():
+    """
+    Register the bot.
+
+    :note: run only once
+    """
+    Mastodon.create_app(
+        'asciimimebot',
+        api_base_url='https://botsin.space',
+        to_file='asciimime_clientcred.secret'
+    )
+
+
+if __name__ == "__main__":
+    PARSER = argparse.ArgumentParser(description='A mastodon bot')
+    PARSER.add_argument('--username', type=str, help='user name to use')
+    PARSER.add_argument('--password', type=str, help='password to use')
+    PARSER.add_argument('--register', action='store_true',
+                        help='register the app (only run once)')
+    ARGS = PARSER.parse_args()
+
+    if ARGS.register:
+        register_app()
+        exit(0)
+
+    acess_token = Mastodon.log_in(username=ARGS.username,
+                                  password=ARGS.password)
